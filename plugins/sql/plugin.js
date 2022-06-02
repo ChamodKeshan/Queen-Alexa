5ff475307126c6cc47efaeb2f309822bcfb8cb91
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-06-02 08:18:58 +0530

    Add files via upload

--- plugins/sql/plugin.js
+++ plugins/sql/plugin.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 Neotro.
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX
+NEOTROX - TEENUHX
 */
 
 const config = require('../../config');
