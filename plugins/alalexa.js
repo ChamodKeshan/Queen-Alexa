8689b7c03793488bb34473a799a3482456cb7ae4
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-06-19 15:04:37 +0530

    Update and rename AIALEXA.js to alalexa.js

--- plugins/alalexa.js
+++ plugins/alalexa.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+NEOTROX
 Wa.me/+94766598862
 */
 //================A==============
